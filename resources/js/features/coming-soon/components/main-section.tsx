1f36215a1ea4cb1159fdbd5086a535c2f6b3c4b2
Xfhreall <farelrizqi377@gmail.com>
2025-07-10 00:17:08 +0700

    feat: responsive design for main-section

--- resources/js/features/coming-soon/components/main-section.tsx
+++ resources/js/features/coming-soon/components/main-section.tsx
@@ -13,22 +13,22 @@ const Main = () => {
         <img
           src="/assets/comingsoon/foto.png"
           alt="foto"
-          className="absolute left-0 object-contain w-1/4 duration-500 ease-in-out top-[40%] md:bottom-14 -rotate-12 hover:rotate-0"
+          className="absolute left-4 md:left-0 object-contain w-1/4 scale-90 duration-500 ease-in-out top-[40%] md:top-14 md:bottom-14 -rotate-12 hover:rotate-0"
         />
         <img
           src="/assets/comingsoon/foto-dua.png"
           alt="foto"
-          className="hidden md:absolute top-20 object-contain w-1/12 duration-500 ease-in-out left-[28%] rotate-[20deg] hover:rotate-0 blur-[3px]"
+          className="hidden md:block absolute left-[28%] top-20 object-contain w-1/12 duration-500 ease-in-out rotate-[20deg] hover:rotate-0 blur-[3px]"
         />
         <img
           src="/assets/comingsoon/foto-dua.png"
           alt="foto"
-          className="absolute object-contain w-1/5 duration-500 ease-in-out hover:rotate-0 right-4 md:-right-12 top-[45%] md:bottom-28 rotate-12 blur-[1px]"
+          className="absolute right-4 md:right-[-3rem] object-contain w-1/5 duration-500 ease-in-out hover:rotate-0 top-[45%] md:top-auto md:bottom-28 rotate-12 blur-[1px] scale-100 md:scale-90"
         />
         <img
           src="/assets/comingsoon/foto.png"
           alt="foto"
-          className="hidden md:absolute object-contain w-32 duration-500 ease-in-out right-[20%] top-32 -rotate-[20deg] hover:rotate-0 blur-[2px]"
+          className="hidden md:block absolute right-[22%] top-32 object-contain w-32 duration-500 ease-in-out -rotate-[20deg] hover:rotate-0 blur-[2px]"
         />
         <div className="flex flex-col max-w-xs px-12 translate-y-14 md:translate-y-0 md:px-8 mx-auto font-sora">
           <img
@@ -66,16 +66,16 @@ const Main = () => {
           </h2>
         </div>
         <div className="z-10 flex flex-col gap-2">
-          <p className="text-2xl text-[#1D5D77] font-extrabold -translate-y-5">
+          <p className="text-2xl md:text-4xl text-[#1D5D77] font-extrabold -translate-y-5">
             Cie... nungguin, ya?
           </p>
-          <div className="mb-12">
-            {/* <img
-                            src="/assets/comingsoon/text-center.svg"
-                            alt="text"
-                            className="mx-auto pointer-events-none scale-90 md:scale-1 select-none"
-                        /> */}
-            <div className="relative mt-[-2vw] font-sora text-5xl">
+          <div className="mb-12 mx-8">
+            <img
+              src="/assets/comingsoon/text-center.svg"
+              alt="text"
+              className="mx-auto pointer-events-none scale-100 select-none hidden md:block"
+            />
+            <div className="relative mt-[-2vw] font-sora text-[2.6em] block md:hidden">
               <h5
                 className="absolute w-full left-0 font-extrabold leading-16"
                 style={{
@@ -104,9 +104,11 @@ const Main = () => {
               window.location.href = '/photo-booth';
             }}
             variant="alt"
-            className="mx-auto cursor-[url('/assets/cursor/photobooth-cursor.svg'),_auto] px-8 py-6 rounded-xl"
+            className="mx-auto -translate-y-6 cursor-[url('/assets/cursor/photobooth-cursor.svg'),_auto] px-8 py-6 rounded-xl"
           >
-            <p className="text-2xl font-extrabold text-white">Klik disini !</p>
+            <p className="text-xl md:text-2xl font-extrabold text-white">
+              Klik disini !
+            </p>
           </Button>
         </div>
       </motion.section>
