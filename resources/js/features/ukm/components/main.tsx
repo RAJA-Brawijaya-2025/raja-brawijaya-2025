cb42929c1996a1545c80358885be2dab2639a6e3
Xfhreall <farelrizqi377@gmail.com>
2025-07-15 11:44:57 +0700

    feat: ukm card comp

---
+++ resources/js/features/ukm/components/main.tsx
@@ -0,0 +1,80 @@
+import { Button } from '@/shared/components/ui/button';
+import { Input } from '@/shared/components/ui/input';
+import { Search } from 'lucide-react';
+import { Kategori } from '@/features/ukm/configs/filter-data';
+import { UkmCard } from './ukm-card';
+
+const UkmMain = () => {
+  const BgOrnamen = () => {
+    return (
+      <>
+        <img
+          src="/assets/fragments/ukm-background-4.svg"
+          alt="orname"
+          className="absolute left-0 -top-24 -scale-x-100 w-1/3"
+        />
+        <img
+          src="/assets/fragments/ukm-background-4.svg"
+          alt="orname"
+          className="absolute right-0 -top-24 w-1/3"
+        />
+        <img
+          src="/assets/fragments/ukm-background.svg"
+          alt="orname"
+          className="absolute left-0 bottom-0 -scale-x-100 w-1/3"
+        />
+        <img
+          src="/assets/fragments/ukm-background.svg"
+          alt="orname"
+          className="absolute right-0 bottom-0 w-1/3"
+        />
+      </>
+    );
+  };
+
+  return (
+    <section className="min-h-screen h-[200vh] relative w-full">
+      <BgOrnamen />
+      <main className="container mx-auto h-max">
+        <div className="flex justify-between items-center">
+          <h2 className="text-gr-05 font-extrabold text-5xl font-sora">
+            Kategori
+          </h2>
+          <div className="relative flex items-center">
+            <Search
+              className="absolute left-1 top-0 mt-2 ml-2 text-muted-foreground"
+              size={20}
+            />
+            <Input
+              placeholder="Cari di sini ygy..."
+              className="w-max pl-10 border-2 border-gr-05"
+            />
+          </div>
+        </div>
+        <div className="flex gap-5 mt-10">
+          {Kategori.map((item, index) => (
+            <Button
+              variant="tertiary"
+              key={index}
+              className={`px-9 py-5 border rounded-xl text-center transition-all
+                                ${
+                                  index === 1
+                                    ? 'bg-gr-05 text-white shadow-[inset_-4px_4px_4px_rgba(255,255,255,0.25),0px_0px_4px_0px_#1D5D77]'
+                                    : 'text-gr-05 shadow-[0px_0px_2px_0px_#1D5D77]'
+                                }`}
+            >
+              <p className="font-bold text-base">{item.label}</p>
+            </Button>
+          ))}
+        </div>
+        <div className="mt-20 grid grid-cols-3 gap-8">
+          {Array.from({ length: 9 }).map((_, idx) => (
+            <UkmCard key={idx} />
+          ))}
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default UkmMain;
--- resources/js/features/ukm/containers/ukm-container.tsx
+++ resources/js/features/ukm/containers/ukm-container.tsx
@@ -1,9 +1,11 @@
 import UkmHero from '../components/hero';
+import UkmMain from '../components/main';
 
 const UkmContainer = () => {
   return (
     <main className="min-h-screen w-full mx-auto">
       <UkmHero />
+      <UkmMain />
     </main>
   );
 };
