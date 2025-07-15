7b51f93ee019b6d8f87735a5848cade504b6b502
syaquiii <msyauqioqi@gmail.com>
2025-07-15 16:18:36 +0700

    style: adjusting gap on mobile

--- resources/js/features/galeri/container/galeri-container.tsx
+++ resources/js/features/galeri/container/galeri-container.tsx
@@ -5,7 +5,7 @@ import GaleriHero from '../components/galeri-hero';
 const GaleriContainer = () => {
   return (
     <section className="relative overflow max-w-screen overflow-hidden min-h-screen  bg-galeri ">
-      <div className="flex flex-col xl:px-20  xl:flex-row px-5 relative z-20 mx-auto container ">
+      <div className="flex flex-col gap-y-40 md:gap-y-44 xl:gap-0 xl:px-20  xl:flex-row px-5 relative z-20 mx-auto container ">
         <GaleriHero />
         <GaleriCard />
       </div>
